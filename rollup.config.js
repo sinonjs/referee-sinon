88032bed00f86e3ba836cb594c131d35c8545abc
Morgan Roderick <morgan@roderick.dk>
2018-03-03 16:55:42 +0000

    Remove UMD wrapper from source
    Instead provide a UMD wrapped version in `dist/` in the package
    published to npm.

---
+++ rollup.config.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var commonjs = require("rollup-plugin-commonjs");
+
+module.exports = {
+    entry: "lib/referee-sinon.js",
+    format: "umd",
+    moduleName: "refereeSinon",
+    plugins: [
+        commonjs({sourceMap: false})
+    ]
+};
